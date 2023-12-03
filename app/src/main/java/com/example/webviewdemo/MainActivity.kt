9c87f568bb78fdb65b34559a88dc7d8fbe3c0605
lanshushui <1256087403@qq.com>
2023-12-03 16:17:07 +0800

    多个进程同时显示webview

--- app/src/main/java/com/example/webviewdemo/MainActivity.kt
+++ app/src/main/java/com/example/webviewdemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.webviewdemo
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 import android.widget.Button
 import com.example.webviewlib.RemoveWebView
 
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<WebView>(R.id.localWebView).loadUrl("https://www.bilibili.com")
         findViewById<Button>(R.id.crash).setOnClickListener {
             findViewById<RemoveWebView>(R.id.webview1).crash()
         }
--- app/src/main/java/com/example/webviewdemo/MyApplication.kt
+++ app/src/main/java/com/example/webviewdemo/MyApplication.kt
@@ -1,9 +1,22 @@
 package com.example.webviewdemo
 
 import android.app.Application
-import android.os.Debug
+import android.content.Context
+import android.os.Build
+import android.webkit.WebView
+
 
 class MyApplication : Application() {
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        //java.lang.RuntimeException
+        //Using WebView from more than one process at once with the same data directory is not supported
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val processName = getProcessName()
+            WebView.setDataDirectorySuffix(processName)
+        }
+    }
     override fun onCreate() {
         super.onCreate()
     }
